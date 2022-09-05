82c5a872b8621e7367389eb024f7e70bdf877888
Bidur Sahoo <78256127+Bidursahoo@users.noreply.github.com>
2022-09-05 15:27:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/*global document*/
+// let tittle = document.getElementById('title')
+// console.log(tittle.innerText);
+// tittle.innerText = "Good Bye world"
+// let message = "Good Bye my User!"
+// tittle.innerHTML = `<p>${message}</p>`
+// tittle.style.color = 'red'
+// let redDiv = document.getElementById("red")
+// let yellowDiv = document.getElementById("yellow")
+// let greenDiv = document.getElementById("green")
+// redDiv.onclick = () => console.log('user choose rock');
+// yellowDiv.onclick = () => console.log('user choose paper');
+// greenDiv.onclick = () => console.log('user choose sizzors');
+const squares = document.querySelectorAll('.colouButton')
+const timeClicked = {'rock':0 , 'paper':0 , 'sizzors':0}
+squares.forEach(name => {
+    name.onclick = () => {
+        timeClicked[name.value] +=1
+        name.innerText = timeClicked[name.value]
+    }
+});
+// squares.forEach(element => {
+//     console.log(element.value);
+// });
